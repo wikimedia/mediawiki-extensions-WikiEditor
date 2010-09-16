d555f7064314666855bbcb8b04096afc05b7f4a6
Trevor Parscal <tparscal@users.mediawiki.org>
2010-09-16 22:20:43 +0000

    Removed unused code.

--- modules/wikiEditor.toc.js
+++ modules/wikiEditor.toc.js
@@ -4,8 +4,5 @@
 
 $( document ).ready( function() {
 	// Add table of contents module
-	$( '#wpTextbox1' )
-		.wikiEditor( 'addModule', 'toc' );
-		// FIXME - should move the ready handler code to the create function so this isn't necissary
-		//.data( 'wikiEditor-context' ).fn.trigger( 'ready' );
+	$( '#wpTextbox1' ).wikiEditor( 'addModule', 'toc' );
 } );
