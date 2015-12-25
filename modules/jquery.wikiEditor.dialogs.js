wikimedia/mediawiki-extensions-WikiEditor

commit 1b797b7f6a276be0e203aeb34dfabf277f1ea44b

Use mediaWiki.message( ... ).plain()
Bug: 42107
Change-Id: Ic17aa1ff18528cec402bef08cace6987529f2782

modules/jquery.wikiEditor.js
@@ -146,9 +146,9 @@ $.wikiEditor = {
 		} else if ( property + 'Msg' in object ) {
 			var p = object[property + 'Msg'];
 			if ( $.isArray( p ) && p.length >= 2 ) {
-				return mediaWiki.msg.apply( mediaWiki.msg, p );
+				return mediaWiki.message.apply( mediaWiki.message, p ).plain();
 			} else {
-				return mediaWiki.msg( p );
+				return mediaWiki.message( p ).plain();
 			}
 		} else {
 			return '';

commit f583469ece2669cc1875fa6f6c10b7a64a378a6b

Use mw.Message#text() instead of plain() in wikiEditor.autoMsg()
* Follows-up 6a73d06 by removing the hardcoded SITENAME regex.
* Follows-up 1b797b7f6a which introduced this regression in the first
  place. This is exactly the reason why mw.msg() defaults to format
  'text' instead of 'plain' without transformations.

Change-Id: I7f1b877710fb6baa45b83054d1e30a24979b37b4

modules/jquery.wikiEditor.dialogs.js
@@ -135,7 +135,7 @@ $.wikiEditor.modules.dialogs = {
 			if ( typeof configuration.modal === 'undefined' ) {
 				configuration.modal = true;
 			}
-			configuration.title = $.wikiEditor.autoMsg( module, 'title' ).replace( /\{\{SITENAME\}\}/i, mw.config.get( 'wgSiteName' ) );
+			configuration.title = $.wikiEditor.autoMsg( module, 'title' );
 			// Transform messages in keys
 			// Stupid JS won't let us do stuff like
 			// foo = { mw.msg( 'bar' ): baz }

modules/jquery.wikiEditor.js
@@ -146,7 +146,7 @@ $.wikiEditor = {
 	},
 
 	/**
-	 * Provides a way to extract messages from objects. Wraps a mw.message( ... ).plain() call.
+	 * Provides a way to extract messages from objects. Wraps a mw.message( ... ).text() call.
 	 *
 	 * @param {Object} object Object to extract messages from
 	 * @param {string} property String of name of property which contains the message. This should be the base name of the
@@ -170,9 +170,9 @@ $.wikiEditor = {
 		} else if ( property + 'Msg' in object ) {
 			p = object[ property + 'Msg' ];
 			if ( $.isArray( p ) && p.length >= 2 ) {
-				return mw.message.apply( mw.message, p ).plain();
+				return mw.message.apply( mw.message, p ).text();
 			} else {
-				return mw.message( p ).plain();
+				return mw.message( p ).text();
 			}
 		} else {
 			return '';