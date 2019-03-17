5f53eb3a2c0c4a8902cb0c4353cdbfb60b696fee
Kunal Mehta <legoktm@member.fsf.org>
2019-03-16 21:44:36 -0700

    Upgrade to new phan
    Bug: T216951
    Change-Id: I05416b7244bf55a600d508ca253fec8410abbbc7

--- .phan/config.php
+++ .phan/config.php
@@ -1,20 +1,20 @@
 <?php
 
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
-		'./../../extensions/EventLogging',
-		'./../../extensions/WikimediaEvents',
+		'../../extensions/EventLogging',
+		'../../extensions/WikimediaEvents',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
-		'./../../extensions/EventLogging',
-		'./../../extensions/WikimediaEvents',
+		'../../extensions/EventLogging',
+		'../../extensions/WikimediaEvents',
 	]
 );
 
